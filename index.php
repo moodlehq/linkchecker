5685b11303c642634cc70c3879a90b1fc9a13103
Aparup Banerjee <aparup@moodle.com>
2014-09-05 11:45:41 +0800

    added proper random sampling of all offline site records. added some images depicting coverage.

--- index.php
+++ index.php
@@ -17,18 +17,32 @@ if (!$isadmin) {
     redirect('http://moodle.net/sites'); //shoo
 }
 $chkurl = optional_param('url', null, PARAM_URL);
-//$siteid = optional_param('id', null, PARAM_URL);
+$siteid = optional_param('siteid', null, PARAM_INT);
+
+$totrecs = $DB->count_records('hub_site_directory');
+
+$img = optional_param('img', null, PARAM_URL);
+if (!is_null($siteid) && !is_null($img)) {
+    $im = getcoverageimg($totrecs, $siteid, true); //get an overlay transparent png for indication.
+    header('content-type: image/png');
+//    header("Content-Length: " . filesize($name));
+    imagepng($im);
+    die();
+}
+
 if ($chkurl !== null) {
     $hdrs = get_headers($chkurl.'/lib/womenslib.php',1);
     if ($hdrs == false) {
         //return some error info.
         $err = error_get_last();
-        echo $err['message'];
+        $result = $err['message'];
     } else {
         //also can check login/forgot_password.php ..for some stuff...
         // anyway the point is humans check and feedback. Devs filter that feedback and transform into rules.. barring AI ofcourse.
-        echo htmlspecialchars($hdrs[0]); // ' Mod:'. $hdrs[3]); //http code and modified.
+        $result = htmlspecialchars($hdrs[0]); // ' Mod:'. $hdrs[3]); //http code and modified.
     }
+//    $result = '200 OK'; //test.
+    echo json_encode(array('result'=>$result, 'siteid' => $siteid, 'imgsrc' => $CFG->wwwroot.'/local/linkchecker/index.php?siteid='.$siteid.'&img=1'));
     die();
 }
 
@@ -38,8 +52,6 @@ $PAGE->set_heading(get_string('registeredmoodlesites', 'local_hub'));
 
 
 echo $OUTPUT->header();
-
-$totrecs = $DB->count_records('hub_site_directory');
 
 $limitfrom = optional_param('limitfrom', rand(1, $totrecs-$limitnum), PARAM_INT);
 
@@ -78,7 +90,7 @@ $failedrecs = $DB->get_records_sql('Select id, url, unreachable, score, fingerpr
     foreach ($failedrecs as $rec) {
         
         $cell = new html_table_cell('Checking..');
-        $cell->attributes = array('id' => $rec->id, 'class' => 'manualcheck', 'url' => $rec->url);
+        $cell->attributes = array('siteid' => $rec->id, 'class' => 'manualcheck', 'url' => $rec->url);
 
         $row = array($rec->id, '<a href="'.$rec->url.'" target="_blank">'.$rec->url.'</a>', $rec->unreachable, $rec->score, (strlen($rec->errormsg)>0)?$rec->errormsg:'fingerprint:'.$rec->fingerprint, $cell);
         $table->data[] = $row;
@@ -91,13 +103,17 @@ $failedrecs = $DB->get_records_sql('Select id, url, unreachable, score, fingerpr
     
     echo '<span class="totrec">Total sites: '. $totrecs. '</span> | <span class="online">Total online &amp; moodley: '.$totsitesonline->onlinesitescount .'</span> | <span class="limitnum">Offline sites loaded in table: '. $limitnum. ' </span> | ';
     echo '<span class="checked">Checked: <span class="chkcnt">0</span></span> | <span class="notfail">not failed: <span class="notfailcnt">0</span></span>  | <span class="fails">Desired Fails: <span class="failcnt">0</span></span> | <span class="percentage">linkchecking fraction (desiredfails/checked): <span class="perc" style="color:#C00;"></span></span>';
-    echo '<br/>Coverage of unmoodle sites:<img class="samplingfailedrecsdistribution" style="width:100%; height:20px;" src="data:image/png;base64,';
+
+    echo '<br/><br/>Test coverage of unmoodley sites:<div style="width:100%"><img class="samplingfailedrecsdistribution" style="border: 1px solid grey;width:800px; height:20px;" src="data:image/png;base64,';
     ob_start();
     imagepng($im);
     $im = ob_get_contents();
     ob_end_clean();
     echo base64_encode($im);
-    echo '" />';
+    echo '"/><br/>Flags for unmoodley sites detected online:<div class="samplingresult" style="width:100%;" ></div></div>';
+
+    echo '<br/>';
+
     echo '<br/><span class="confidence">Default sample size 400 is for a 95% (+/-5%) confidence test.  Adjustable in url by appending "?limitnum=xxx". Please remember to adjust your linkchecking percentage after human checking and raise moodley sites from here to developers.</span>';
 
     echo $htmltable ;
@@ -115,28 +131,34 @@ $jsscr = <<<js
             $(".manualcheck").each(function(index, element){
                 element.innerHTML = 'Checking, sent to server, awaiting response..';
                 $.get('index.php',
-                    { url: this.getAttribute('url') },
+                    { url: this.getAttribute('url'),
+                      siteid: this.getAttribute('siteid')
+                    },
                     function(responseText) {
-                    console.log(responseText);
-                    element.innerHTML = responseText;
-                    chkcnt = $(".chkcnt").html();
-                    chkcnt++;
-                    $(".chkcnt").html(chkcnt);
-                    if (responseText.indexOf("200 OK") >= 0) {
-                        element.innerHTML = '<span style="color:#ff0000">' + responseText + ' Human verification will be good here.</span>';
-                        notfailcnt = $(".notfailcnt").html();
-                        notfailcnt++;
-                        $(".notfailcnt").html(notfailcnt);
-                    } else {
-                        failcnt = $(".failcnt").html();
-                        failcnt++;
-                        $(".failcnt").html(failcnt);
-                        console.log("desired fail count:"+failcnt);
-                        percentage = failcnt / chkcnt;
-                        console.log("linkchecker percentage(%):"+percentage);
-                        $(".perc").html(percentage);
+                        responseText = jQuery.parseJSON(responseText);
+                        console.log(responseText);
+                        element.innerHTML = responseText.result;
+                        chkcnt = $(".chkcnt").html();
+                        chkcnt++;
+                        $(".chkcnt").html(chkcnt);
+                        if (responseText.result.indexOf("200 OK") >= 0) {
+                            element.innerHTML = '<span style="color:#ff0000">' + responseText.result + ' Human verification preferred.</span>';
+                            notfailcnt = $(".notfailcnt").html();
+                            notfailcnt++;
+                            $(".notfailcnt").html(notfailcnt);
+                            var samplingrs = $(".samplingresult");
+                            $(".samplingresult").append('<img style="border: 1px solid grey;width:800px; height:20px;float: left; position: absolute;" src="'+ responseText.imgsrc + '" />');
+                        } else {
+                            failcnt = $(".failcnt").html();
+                            failcnt++;
+                            $(".failcnt").html(failcnt);
+                            console.log("desired fail count:"+failcnt);
+                            percentage = failcnt / chkcnt;
+                            console.log("linkchecker percentage(%):"+percentage);
+                            $(".perc").html(percentage);
+                        }
                     }
-                });
+                );
             });
     }
     linkchecker();
@@ -147,25 +169,41 @@ echo $jsscr; // bloody yui
 
 echo $OUTPUT->footer();
 
-function getcoverageimg($totrecs, $randomrecordids, $highlightrecs=null) {
+function getcoverageimg($totrecs, $randomrecordids, $highlightrecs=false) {
     core_php_time_limit::raise(300);
-    $width = 18000; $height = 10; $padding = 10;
+    $width = 18000; $height = 20; $padding = 5;
     $column_width = $width / $totrecs ;
     $im        = imagecreate($width,$height);
-    $gray      = imagecolorallocate ($im,0xcc,0xcc,0xcc);
-    $gray_lite = imagecolorallocate ($im,0xee,0xee,0xee);
-    $gray_dark = imagecolorallocate ($im,0x7f,0x7f,0x7f);
+    imagesavealpha($im, true);
+    //setting completely transparent color
+    $transparent = imagecolorallocatealpha($im, 0, 0, 0, 127);
+    $gray      = imagecolorallocate ($im,0x4c,0x4c,0x4c);
+    $gray_lite = imagecolorallocate ($im,0x7e,0x7e,0x7e);
+    $gray_dark = imagecolorallocate ($im,0x05,0x05,0x05);
     $white     = imagecolorallocate ($im,0xff,0xff,0xff);
-    imagefilledrectangle($im,0,0,$width,$height,$white);
+    $red     = imagecolorallocate ($im,0xee,0x10,0x10);
+
     $maxv = 1;
-    for($i=0;$i<$totrecs;$i++)
-    {
-        $column_height = 1+ (9* (int) in_array($i, $randomrecordids));
-        $x1 = $i*$column_width;
+    if (is_array($randomrecordids)) {
+        imagefilledrectangle($im,0,0,$width,$height,$white);
+        //left in some column ht code (c&p)
+        for($i=0;$i<$totrecs;$i++)
+        {
+            $column_height = (10* ((int) in_array($i, $randomrecordids)));
+            $x1 = $i*$column_width;
+            $y1 = $height-$column_height;
+            $x2 = (($i+1)*$column_width)-$padding;
+            $y2 = $height;
+            imagefilledrectangle($im,$x1,$y1,$x2,$y2,$highlightrecs?$red:$gray);
+        }
+    } else if (is_int ($randomrecordids)) { //highlight - transparent fill
+        imagefilledrectangle($im,0,0,$width,$height,$transparent);
+        $column_height = $height;
+        $x1 = $randomrecordids*$column_width;
         $y1 = $height-$column_height;
-        $x2 = (($i+1)*$column_width)-$padding;
+        $x2 = (($randomrecordids+1)*$column_width)-$padding;
         $y2 = $height;
-        imagefilledrectangle($im,$x1,$y1,$x2,$y2,$gray);
+        imagefilledrectangle($im,$x1,$y1,$x2,$y2,$highlightrecs?$red:$gray);
     }
     return $im;
 }
